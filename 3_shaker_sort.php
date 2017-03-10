216c67a331d9d786e1a5002eec40dcef3819a4ba
Den-action <Den-action@yandex.ru>
2017-03-10 17:16:44 +0200

    Refactored 3_shaker_sort.php
    Refactored 3_shaker_sort.php

--- 3_shaker_sort.php
+++ 3_shaker_sort.php
@@ -4,20 +4,27 @@ $massive = [5,2,1,3,9,0,4,6,8,7];
 $maxIndex = count($massive) - 1;
 $direction = true;
 $flag = true;
+$minIndex = 0;
 while ($flag) {
     $flag = false;
-    for ($index = $direction ? $minIndex : $maxIndex - 1; ; $index = $direction ? ++$index : --$index) {
+    if ($direction) {
+        $index = $minIndex;
+    }
+    else {
+        $index = $maxIndex - 1;
+    }
+    while (true) {
         if ($massive[$index] > $massive[$index + 1]) {
             list($massive[$index], $massive[$index + 1]) = array($massive[$index + 1], $massive[$index]);
             $flag = true;
         }
-        if ($direction && $index == $maxIndex -1)
+        if (($direction && $index == $maxIndex -1) || (!$direction && $index == $minIndex) )
             break;
-        elseif (!$direction && $index == $minIndex)
-            break;
+        if ($direction)
+            ++$index;
+        else
+            --$index;
     }
-    $minIndex = $direction ? $minIndex : ++$minIndex;
-    $maxIndex = $direction ? --$maxIndex : $maxIndex;
     $direction = !$direction;
 }
 
